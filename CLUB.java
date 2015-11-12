f3610818db9e904c50d6ab0c125155ba41ea52e7
comptski <brendan.mccart@stkentigernsacademy.org.uk>
2015-11-12 08:56:33 +0000

    update

--- CLUB.java
+++ CLUB.java
@@ -28,10 +28,17 @@ public class CLUB
 
     private void setUpMemberList() throws IOException
     {
+        // First user message
+        System.out.println("ScotFit Club: Membership BMI update/n");
+        System.out.println("** Preparing to read data file.");
+
         // read file, fetch data as String array containing the rows
         String[] dataRows = bmiFile.readCSVtable();
         // calculate the number of member rows, skip headings
         noOfMembers = dataRows.length - 1;
+
+        // update user with number of rows with membership details
+        System.out.println("** " + noOfMembers + " rows read.\n\n");
 
         // prepare array for members
         memberList = new MEMBER[noOfMembers];
@@ -44,15 +51,22 @@ public class CLUB
     }
 
     public void displayMembers() {
+        // Heading for the display
+        System.out.println("A listing of all applicants for the next year\n");
+        // results
         for  (int i = 0; i < noOfMembers; i++) {
             memberList[i].displayDetails();
         }
+        // 2 blank line to separate this report from others.
+        System.out.print("\n\n\n");
     }
 
     public void countOKbmi() throws IOException
     {
         // *prepare a String to write data to disc
         String fileContent = "";
+
+        System.out.println("A report of members within ideal BMI\n");
 
         // start the count
         int count = 0;
@@ -64,24 +78,27 @@ public class CLUB
             {
                 // add 1 to count: for OK bmi
                 count = count +1;
-                
                 // *display the details for the member
                 memberList[i].displayDetails();
 
                 // *use new line to separate rows in csv file, after 1st line
-                if (i>0) {
+                if (count>1) 
+                {
                     fileContent = fileContent.concat("\n");
                 }
                 // *join on next line of data for writing to file
                 fileContent = fileContent.concat(memberList[i].writeDetails());
             }
         }
-
         // display the final count: bmi
-        System.out.println("Total for OK BMI is : " + count);
+        System.out.println("\n Total for OK BMI is : " + count);
+        // A blank line to separate this report from others.
+        System.out.println();
 
-        // *send for writing to file
+        // *send for writing to file as a string containing all data
+        System.out.println("** Preparing to write data file.");
         resultFile.writeCSVtable(fileContent);
+        System.out.println("** File written and closed.");
     }
 
     public static void main(String[] args)  throws IOException
